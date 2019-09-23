e53c8c0252270e585a2bc5484639b2fce445e38a
Edward Lu <e.lu616@gmail.com>
2019-09-23 12:25:20 -0400

    pt1

---
+++ bash.js
@@ -0,0 +1,8 @@
+process.stdout.write('prompt > ');
+
+//The stdin 'data' event fires after a user types in a new line
+process.stdin.on('data', (data) => {
+  const cmd = data.toString().trim(); // remove the newline
+  process.stdout.write('You typed: ' + cmd);
+  process.stdout.write('\nprompt > ');
+});
